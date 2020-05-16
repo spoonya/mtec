56658230180ffeb6e81057b437893d3f2ce1f3e7
nomad8t <serega18t@tut.by>
2020-05-16 21:55:03 +0300

    Index commit: spec cards, resources

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
 jQuery(function ($) {
-    
+    $('#owl1').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
 });
