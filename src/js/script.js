e96d299ba84b59ac4eeda1a0deb035487f1bf726
nomad8t <serega18t@tut.by>
2020-05-27 13:22:10 +0300

    Новости

--- src/js/script.js
+++ src/js/script.js
@@ -14,5 +14,16 @@ jQuery(function ($) {
                 items:4
             }
         }
-    })
+    });
+
+    // Pagination
+    $('.pag__next').click(function(){
+        $('.pag').find('pag__num.active').next().addClass('active');
+        $('.pag').find('pag__num.active').prev().removeClass('active');
+    });
+
+    $('.pag__prev').click(function(){
+        $('.pag').find('pag__num.active').prev().addClass('active');
+        $('.pag').find('pag__num.active').next().removeClass('active');
+    });
 });
