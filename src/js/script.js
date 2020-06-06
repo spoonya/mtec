f6cd51378d762d443c84ab792002ddbc0799a972
Ogur4iK <koryavi2014@mail.ru>
2020-06-06 13:51:42 +0300

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,65 @@
 jQuery(function ($) {
     $('#owl1').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
     });
 
     // Pagination
-    $('.pag__next').click(function(){
+    $('.pag__next').click(function () {
         $('.pag').find('pag__num.active').next().addClass('active');
         $('.pag').find('pag__num.active').prev().removeClass('active');
     });
 
-    $('.pag__prev').click(function(){
+    $('.pag__prev').click(function () {
         $('.pag').find('pag__num.active').prev().addClass('active');
         $('.pag').find('pag__num.active').next().removeClass('active');
     });
 
     //Fotorama
-    jQuery(function($){var fotorama = $('.fotorama').fotorama({shadows: false, nav: 'thumbs', fit: 'cover', thumbwidth: 100, thumbheight: 70, loop: false, arrows: 'always', width: '100%', maxwidth: '100%', ratio: 16/10, allowfullscreen: 'native'}).data('fotorama'); $('.fotorama').on('fotorama:fullscreenenter', function (e, fotorama) {fotorama.setOptions({fit: 'contain'});});$('.fotorama').on('fotorama:fullscreenexit', function (e, fotorama) {fotorama.setOptions({fit: 'cover'});});})
+    var fotorama = $('.fotorama').fotorama({
+        shadows: false,
+        nav: 'thumbs',
+        fit: 'cover',
+        thumbwidth: 100,
+        thumbheight: 70,
+        loop: false,
+        arrows: 'always',
+        width: '100%',
+        maxwidth: '100%',
+        ratio: 16 / 10,
+        allowfullscreen: 'native'
+    }).data('fotorama');
+    $('.fotorama').on('fotorama:fullscreenenter', function (e, fotorama) {
+        fotorama.setOptions({
+            fit: 'contain'
+        });
+    });
+    $('.fotorama').on('fotorama:fullscreenexit', function (e, fotorama) {
+        fotorama.setOptions({
+            fit: 'cover'
+        });
+    });
+
+    //Menu
+    $(".hdr__menu-item")
+        .mouseenter(function () {
+            var sub = $(this).children('.sub__menu');
+            sub.slideDown(200);
+        })
+        .mouseleave(function () {
+            var sub = $(this).children('.sub__menu')
+            sub.slideUp(200);
+        });
 });
